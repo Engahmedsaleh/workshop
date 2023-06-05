34a3fd32cd916cfb951e23cd47a27d492d165a3c
Engahmedsalegit commit -m newupdate[200~ git config --global user.email engahmedsaleh179@gmail.com  git config --global user.name Engahmedsale <engahmedsaleh179@gmail.com>
2023-06-05 14:14:14 +0200

    newupdate

---
+++ js/index.js
@@ -0,0 +1,128 @@
+// // $(function () {
+// //     $(".supmenu").hide()
+// //     $(".color").click(function () {
+// //         $(".supmenu").fadeToggle()
+// //     })
+// // })
+
+// function toggleMenu(dv){
+//    myShowMenu = dv.getAttribute('data-show-info');
+//    if(myShowMenu == 'closed'){
+//     document.querySelector('.supmenu').style.display = 'block';
+//     dv.setAttribute('data-show-info' , 'show');
+//    }else{
+//     document.querySelector('.supmenu').style.display = 'none';
+//     dv.setAttribute('data-show-info' , 'closed');
+//    }
+  
+// }
+
+
+
+// function toggleMenu1(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info-1');
+//     if(myShowMenu == 'closed-1'){
+//      document.querySelector('.supmenu1').style.display = 'block';
+//      dv1.setAttribute('data-show-info-1' , 'show');
+//     }else{
+//      document.querySelector('.supmenu1').style.display = 'none';
+//      dv1.setAttribute('data-show-info-1' , 'closed-1');
+//     }
+   
+//  }
+
+
+//  function toggleMenu2(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info-1');
+//     if(myShowMenu == 'closed-1'){
+//      document.querySelector('.supmenu2').style.display = 'block';
+//      dv1.setAttribute('data-show-info-1' , 'show');
+//     }else{
+//      document.querySelector('.supmenu2').style.display = 'none';
+//      dv1.setAttribute('data-show-info-1' , 'closed-1');
+//     }
+   
+//  }
+
+//  function toggleMenu3(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info-1');
+//     if(myShowMenu == 'closed-1'){
+//      document.querySelector('.supmenu3').style.display = 'block';
+//      dv1.setAttribute('data-show-info-1' , 'show');
+//     }else{
+//      document.querySelector('.supmenu3').style.display = 'none';
+//      dv1.setAttribute('data-show-info-1' , 'closed-1');
+//     }
+   
+//  }
+
+//  function toggleMenu4(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info-1');
+//     if(myShowMenu == 'closed'){
+//      document.querySelector('.supmenu4').style.display = 'block';
+//      dv1.setAttribute('data-show-info-1' , 'show');
+//     }else{
+//      document.querySelector('.supmenu4').style.display = 'none';
+//      dv1.setAttribute('data-show-info-1' , 'closed');
+//     }
+   
+//  }
+
+//  function toggleMenu5(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info-1');
+//     if(myShowMenu == 'closed'){
+//      document.querySelector('.supmenu5').style.display = 'block';
+//      dv1.setAttribute('data-show-info-1' , 'show');
+//     }else{
+//      document.querySelector('.supmenu5').style.display = 'none';
+//      dv1.setAttribute('data-show-info-1' , 'closed');
+//     }
+   
+//  }
+
+//  function toggleMenu6(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info-1');
+//     if(myShowMenu == 'closed'){
+//      document.querySelector('.supmenu6').style.display = 'block';
+//      dv1.setAttribute('data-show-info-1' , 'show');
+//     }else{
+//      document.querySelector('.supmenu6').style.display = 'none';
+//      dv1.setAttribute('data-show-info-1' , 'closed');
+//     }
+   
+//  }
+
+// // -------------------------------------------------------------
+
+
+//  function toggleMenu10(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info');
+//     if(myShowMenu == 'closed'){
+//      document.querySelector('.supmenu-mobil').style.display = 'block';
+//      dv1.setAttribute('data-show-info' , 'show');
+//     }else{
+//      document.querySelector('.supmenu-mobil').style.display = 'none';
+//      dv1.setAttribute('data-show-info' , 'closed');
+//     }
+   
+//  }
+
+//  function toggleMenu11(dv1){
+//     myShowMenu = dv1.getAttribute('data-show-info');
+//     if(myShowMenu == 'closed'){
+//      document.querySelector('.menu-mobil').style.display = 'block';
+//      dv1.setAttribute('data-show-info' , 'show');
+//     }else{
+//      document.querySelector('.menu-mobil').style.display = 'none';
+//      dv1.setAttribute('data-show-info' , 'closed');
+//     }
+   
+//  }
+
+ 
+// //  let alldivs = document.querySelectorAll("li a")
+// //  el.onclick = function(){
+// //     alldivs.forEach(function(el){
+        
+// //     })
+// //  }
